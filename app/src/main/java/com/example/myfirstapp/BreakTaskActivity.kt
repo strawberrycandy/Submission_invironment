2efd1104b991208d831e77f4051d4bca727bafca
yamashun200609010612-tech <yamashun200609010612@gmail.com>
2025-10-22 09:48:24 +0900

    通知と音声のみ実行完了

---
+++ app/src/main/java/com/example/myfirstapp/BreakTaskActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BreakTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_break_task)
+    }
+}
